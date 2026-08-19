94765dc1dfbac28d9cd93f95d797da439b791bde
Melwin K S <melwinks75@gmail.com>
2026-08-19 20:44:11 +0530

    Footer edited

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -8,7 +8,7 @@ export default function Footer() {
           target="_blank"
           className="text-blue-600 hover:underline"
         >
-          Rathan Kumar
+          Melwin K S
         </a>
       </p>
 
