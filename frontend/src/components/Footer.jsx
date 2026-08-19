606358f383a88ffec8acefc28964ea56e9e38725
Melwin K S <melwinks75@gmail.com>
2026-08-19 20:41:56 +0530

    Footer edited

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
       <p className="flex flex-col items-center gap-1">
         Developed with <span className="text-red-500">❤️</span> by{" "}
         <a
-          href="https://www.linkedin.com/in/rathan-kumar492"
+          href="https://github.com/melwinks05"
           target="_blank"
           className="text-blue-600 hover:underline"
         >
@@ -14,15 +14,15 @@ export default function Footer() {
 
       <div className="flex flex-col items-center gap-1 mt-2">
         <a
-          href="mailto:developer.rathan@gmail.com"
+          href="mailto:melwinks75@gmail.com"
           className="text-blue-600 hover:underline"
         >
-          developer.rathan@gmail.com{" "}
+          melwinks75@gmail.com{" "}
         </a>
 
         <span className="flex items-center gap-1 text-gray-500 text-sm">
           <i className="bi bi-geo-alt-fill" style={{ fontSize: "20px" }}></i>
-          Mumbai, India
+          Mangalore, India
         </span>
       </div>
     </footer>
